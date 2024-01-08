a7f861582d7617382d3f1fcc14c0e380b6f976cd
1803063soumit <1803063soumit@gmail.com>
2024-01-08 19:20:54 +0600

    A flask app is buid. Project is done.

--- src/BreastCancerClassification/pipeline/predict.py
+++ src/BreastCancerClassification/pipeline/predict.py
@@ -7,6 +7,7 @@ import os
 class PredictionPipeline:
     def __init__(self, filename):
         self.filename = filename
+    def predict(self):
         # load model
         model = load_model(os.path.join("models", "trained_model.h5"))
 
@@ -14,14 +15,16 @@ class PredictionPipeline:
         test_image = image.load_img(imagename, target_size=(224, 224))
         test_image = image.img_to_array(test_image)
         test_image = np.expand_dims(test_image, axis=0)
-        result = np.argmax(model.predict(test_image), axis=1)
-        print(result)
+        test_image /= 255.
+        predictions = model.predict(test_image)
+        result = (predictions > 0.5).astype(int)
+        print("--------Prediction------->", predictions)
 
         if result[0] == 1:
-            prediction = 'Healthy'
+            prediction = 'Malignant'
             return [{"image": prediction}]
         else:
-            prediction = 'Coccidiosis'
+            prediction = 'Benign'
             return [{"image": prediction}]
 
         # # Evaluate the model on the test set
